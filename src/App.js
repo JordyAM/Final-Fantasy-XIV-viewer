a0fd5d166880a99a1d0d3c8d9db62e14fef32c61
JordyAM <JAlexanderMcLean@Gmail.com>
2022-03-03 17:49:46 -0500

    fixed my first fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react'
+
+
 
 function App() {
+
+  const [data, setData] = useState([{}]);
+  
+useEffect(() => {
+
+})
+
+
+useEffect(() => {fetch('https://xivapi.com/character/search?name=Dural Quartermain')
+.then((res) => res.json())
+.then((res) => {
+  setData(res);
+  console.log(data);
+
+})
+.catch(console.error);}, []);
+
+if (!data) {
+  return <div>Loading...</div>
+}
+
+
   return (
     <div >
       
