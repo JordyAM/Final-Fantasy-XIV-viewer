d4ee3e07ae4c56f672fc7e8075d6995ec8158e38
JordyAM <JAlexanderMcLean@Gmail.com>
2022-03-07 07:30:57 -0500

    map not working, minions are request locked

--- src/components/Minions.js
+++ src/components/Minions.js
@@ -4,15 +4,11 @@ import ReactTooltip from 'react-tooltip';
 function Minions({charData}) {
     return (
         <div>
-          {charData.map(data => (
-            <img src={data.Minions.Icon} data-tip data-for="minionTip"/>
-            <ReactTooltip id="minionTip">{data.Minions.Name}</ReactTooltip>
-
-           
+          {/* {charData.map(data => (
+            <img src={data.Minions.Icon} />
+            // <ReactTooltip id="minionTip">{data.Minions.Name}</ReactTooltip>
 ))
-        
-    
-}
+} */}
 </div>
     )}
 export default Minions;
