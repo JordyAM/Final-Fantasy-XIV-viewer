79de01342d97addee4815ed3b4241aa2ec1dff57
JordyAM <JAlexanderMcLean@Gmail.com>
2022-03-08 16:30:55 -0500

    cleaning up code again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import {Route, Routes, Link} from 'react-router-dom'
 import React, {useState,useEffect} from 'react'
-// import Minions from './components/Minions';
 import Jobs from './components/Jobs';
 import About from './components/About';
 import Character from './components/Character';
 import SearchID from './components/SearchID';
 function App() {
 
-  // const [data, setData] = useState([]);
   const [charData, setCharData] = useState(null);
   const [charID, setCharID] = useState('5030778');
   
@@ -48,7 +46,6 @@ if (!charData) {
       <nav className='navigation'>
         <h1><Link to="/">FFXIV Character Viewer</Link></h1>
         <h1><Link to="/components/Jobs">Classes/Jobs</Link></h1>
-        {/* <h1><Link to="/components/Minions">Minions</Link></h1> */}
         <h1><Link to="./components/About">About</Link></h1>
         </nav>
       
@@ -63,7 +60,6 @@ if (!charData) {
       <Routes>
         <Route path="/" element={<Character charData={charData} />}/>
         <Route path='/components/Jobs' element={<Jobs charData={charData}/>} />
-        {/* <Route path='/components/Minions' element={<Minions charData={charData}/>} />  */}
         <Route path='/components/About' element= {<About />} />
        </Routes>
       </main>
